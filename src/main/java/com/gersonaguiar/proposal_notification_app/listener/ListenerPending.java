0f291f3379da1b2de9ba1d6bed9f5c53d537b7fc
ggersonmg <gersoncafilho@gmail.com>
2025-02-16 19:16:59 -0300

    listener message by queue RabbitMQ and send SMS use Amazon SQS it's working.

--- src/main/java/com/gersonaguiar/proposal_notification_app/listener/ListenerPending.java
+++ src/main/java/com/gersonaguiar/proposal_notification_app/listener/ListenerPending.java
@@ -4,16 +4,22 @@ import com.gersonaguiar.proposal_notification_app.domain.Proposal;
 import com.gersonaguiar.proposal_notification_app.service.NotificationSnsService;
 import com.gersonaguiar.proposal_notification_app.util.MessageContants;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ListenerPending {
 
+    @Autowired
     private NotificationSnsService notificationSnsService;
 
     @RabbitListener(queues = "${rabbitmq.queue.proposal.pending}")
     public void proposalPending(Proposal proposal) {
-        String message = String.format(MessageContants.PROPOSAL_NOTIFICATION_TITLE,  proposal.getUser().getFirstName(), proposal.getUser().getLastName());
-        notificationSnsService.sendNotification(message);
+        String userName = (proposal.getUser() != null) ? proposal.getUser().getFirstName() : "Unknown User";
+        String message = String.format(MessageContants.PROPOSAL_NOTIFICATION_TITLE,  userName);
+
+        String cellPhone = "+5538";
+
+        notificationSnsService.sendNotification(cellPhone, message);
     }
 }
--- src/main/java/com/gersonaguiar/proposal_notification_app/service/NotificationSnsService.java
+++ src/main/java/com/gersonaguiar/proposal_notification_app/service/NotificationSnsService.java
@@ -1,8 +1,20 @@
 package com.gersonaguiar.proposal_notification_app.service;
 
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.model.PublishRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NotificationSnsService {
 
-    public void sendNotification(String message) {
+    @Autowired
+    private AmazonSNS amazonSSN;
 
+    public void sendNotification(String cellPhone, String message) {
+        PublishRequest publishRequest = new PublishRequest()
+                .withMessage(message)
+                .withPhoneNumber(cellPhone);
+        amazonSSN.publish(publishRequest);
     }
 }
