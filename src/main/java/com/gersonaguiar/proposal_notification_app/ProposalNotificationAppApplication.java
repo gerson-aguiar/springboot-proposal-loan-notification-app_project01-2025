2b18873358d22da893af36c342ed91d48f8b374d
ggersonmg <gersoncafilho@gmail.com>
2025-02-16 18:13:14 -0300

    first commit

---
+++ src/main/java/com/gersonaguiar/proposal_notification_app/ProposalNotificationAppApplication.java
@@ -0,0 +1,13 @@
+package com.gersonaguiar.proposal_notification_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProposalNotificationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProposalNotificationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gersonaguiar/proposal_notification_app/ProposalNotificationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gersonaguiar.proposal_notification_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProposalNotificationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
